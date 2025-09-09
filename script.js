a633f17a95c926b2ab54dfcfdda0fd80589d6d01
games0000 <140877895+games0000@users.noreply.github.com>
2025-09-10 02:30:55 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// 图片轮播
-let current = 0;
-const slides = document.querySelectorAll('.hero img');
-setInterval(() => {
-  slides[current].classList.remove('active');
-  current = (current + 1) % slides.length;
-  slides[current].classList.add('active');
-}, 4000);
